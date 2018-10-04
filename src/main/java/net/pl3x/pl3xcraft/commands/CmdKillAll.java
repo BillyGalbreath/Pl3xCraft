69c6bd00111f1184cd0f6ca8e9404d0a4d40c5a3
DwDMadMac <anthonymmacallister@gmail.com>
2018-10-04 15:49:58 -0400

    Removed unused import

--- src/main/java/net/pl3x/pl3xcraft/commands/CmdKillAll.java
+++ src/main/java/net/pl3x/pl3xcraft/commands/CmdKillAll.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
-import org.bukkit.entity.Player;
 
 public class CmdKillAll implements TabExecutor {
 
