efbac8554f8204390bc7c1930a3c7ea60553e1e3
William Blake Galbreath <blake.galbreath@gmail.com>
2019-07-12 02:43:47 -0500

    Wrap ChunkTooLargeEvent listener in 2 second delayed task

--- src/main/java/net/pl3x/pl3xcraft/listener/ChunkListener.java
+++ src/main/java/net/pl3x/pl3xcraft/listener/ChunkListener.java
@@ -1,5 +1,6 @@
 package net.pl3x.pl3xcraft.listener;
 
+import net.pl3x.pl3xcraft.Pl3xCraft;
 import net.pl3x.pl3xcraft.configuration.Lang;
 import net.pl3x.purpur.event.ChunkTooLargeEvent;
 import org.bukkit.Location;
@@ -7,6 +8,7 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,20 +16,24 @@ import java.util.stream.Collectors;
 public class ChunkListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onChunkTooLarge(ChunkTooLargeEvent event) {
-        String message = "&7[&4WARNING&7] &cDetected a " + (event.isSaving() ? "save" : "load") + " of an over-sized chunk! Possible duplication attempt!";
+        new BukkitRunnable() {
+            public void run() {
+                String message = "&7[&4WARNING&7] &cDetected a " + (event.isSaving() ? "save" : "load") + " of an over-sized chunk! Possible duplication attempt!";
 
-        Location loc = event.getLocation();
-        if (loc != null) {
-            message += "\n&cNear coords&3: &e" + loc.getWorld().getName() + " " + loc.getX() + "&7,&e" + loc.getY() + "&7,&e" + loc.getZ();
+                Location loc = event.getLocation();
+                if (loc != null) {
+                    message += "\n&cNear coords&3: &e" + loc.getWorld().getName() + " " + loc.getX() + "&7,&e" + loc.getY() + "&7,&e" + loc.getZ();
 
-            List<String> nearby = loc.getNearbyPlayers(32).stream()
-                    .map(HumanEntity::getName)
-                    .collect(Collectors.toList());
-            message += "\n&cPlayers nearby&3: &e" + (String.join("&7, &e", nearby));
-        } else {
-            message += "\n&cNear coords&e: &e" + (event.getChunkX() << 4) + "&7,&e?&e,&e" + (event.getChunkZ() << 4);
-        }
+                    List<String> nearby = loc.getNearbyPlayers(32).stream()
+                            .map(HumanEntity::getName)
+                            .collect(Collectors.toList());
+                    message += "\n&cPlayers nearby&3: &e" + (String.join("&7, &e", nearby));
+                } else {
+                    message += "\n&cNear coords&e: &e" + (event.getChunkX() << 4) + "&7,&e?&e,&e" + (event.getChunkZ() << 4);
+                }
 
-        Lang.broadcast(message);
+                Lang.broadcast(message);
+            }
+        }.runTaskLater(Pl3xCraft.getInstance(), 20L);
     }
 }
