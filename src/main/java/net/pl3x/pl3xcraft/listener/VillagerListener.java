3445d121d0ed2e31efc9625cc79cf0918161d184
William Blake Galbreath <blake.galbreath@gmail.com>
2019-10-07 00:25:44 -0500

    Reset villagers back to emerald trades

--- src/main/java/net/pl3x/pl3xcraft/listener/VillagerListener.java
+++ src/main/java/net/pl3x/pl3xcraft/listener/VillagerListener.java
@@ -1,18 +1,20 @@
 package net.pl3x.pl3xcraft.listener;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
-
-import java.util.stream.Collectors;
 
 public class VillagerListener implements Listener {
     @EventHandler
     public void onVillagerAcquireTrades(VillagerAcquireTradeEvent event) {
-        /*MerchantRecipe recipe = event.getRecipe();
+        /*
+        MerchantRecipe recipe = event.getRecipe();
 
         // replace emerald ingredients with gold ingot
         recipe.setIngredients(recipe.getIngredients().stream()
@@ -32,6 +34,27 @@ public class VillagerListener implements Listener {
             newRecipe.setIngredients(recipe.getIngredients());
 
             event.setRecipe(newRecipe);
-        }*/
+        }
+        */
+    }
+
+    @EventHandler
+    public void onVillagerOpenTrades(InventoryOpenEvent event) {
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (!(holder instanceof Villager)) {
+            return;
+        }
+        Villager villager = (Villager) holder;
+        for (MerchantRecipe recipe : villager.getRecipes()) {
+            if (recipe.getResult().getType() == Material.EMERALD) {
+                return; // has emerald trade
+            }
+            for (ItemStack ingredient : recipe.getIngredients()) {
+                if (ingredient.getType() == Material.EMERALD) {
+                    return; // has emerald trade
+                }
+            }
+        }
+        villager.resetOffers();
     }
 }
